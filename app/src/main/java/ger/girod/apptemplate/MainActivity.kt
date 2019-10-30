a58aa0689d3a0fa35cadde2ad4bfc92abdc13c80
german girod <germangirod@germans-MacBook-Pro.local>
2019-10-30 09:44:00 -0300

    initialize project

---
+++ app/src/main/java/ger/girod/apptemplate/MainActivity.kt
@@ -0,0 +1,12 @@
+package ger.girod.apptemplate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
